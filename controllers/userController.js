54c3212fbaa259226c5ad5b238e9937b996d179b
siddhardik <siddharthabhunia2001@gmail.com>
2023-09-26 19:07:34 +0530

    Login Complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,9 +75,48 @@ exports.getAllUsers = async (req, res) => {
   };
 
 
-//Login 
-exports.loginController = () => {
-};
+
+
+//login
+exports.loginController = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      //validation
+      if (!email || !password) {
+        return res.status(401).send({
+          success: false,
+          message: "Please provide email or password",
+        });
+      }
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res.status(200).send({
+          success: false,
+          message: "This email is not registerd ",
+        });
+      }
+      //password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).send({
+          success: false,
+          message: "Invalid username or password",
+        });
+      }
+      return res.status(200).send({
+        success: true,
+        messgae: "login successfully",
+        user,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        success: false,
+        message: "Error In Login Callcback",
+        error,
+      });
+    }
+  };
 
 
 
